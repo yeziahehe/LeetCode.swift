8fd7c4005074b7669a1626ff849d3c367fa113ef
叶帆 <yeziahehe@gmail.com>
2017-10-10 01:11:43 +0800

    [053][Easy][Array][Divide and Conquer][Dynamic Programming] : Maximum Subarray.

--- LeetCode.swift/Solution/053_MaximumSubarray/MaximumSubarray.swift
+++ LeetCode.swift/Solution/053_MaximumSubarray/MaximumSubarray.swift
@@ -5,7 +5,7 @@
 //  Created by 叶帆 on 2017/10/9.
 //
 
-/*
+/**
 Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
 
 For example, given the array [-2,1,-3,4,-1,2,1,-5,4],
