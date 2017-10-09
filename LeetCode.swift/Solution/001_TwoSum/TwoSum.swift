fc248d077053423044553d10b50a903bf5d0b8ae
叶帆 <yeziahehe@gmail.com>
2017-10-10 01:12:45 +0800

    [001][Easy][Array][Hash Table]: Two Sum.

--- LeetCode.swift/Solution/001_TwoSum/TwoSum.swift
+++ LeetCode.swift/Solution/001_TwoSum/TwoSum.swift
@@ -5,7 +5,7 @@
 //  Created by 叶帆 on 2017/10/9.
 //
 
-/*
+/**
  Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  
  You may assume that each input would have exactly one solution, and you may not use the same element twice.
