f41be498609c1443307445cdf3d70a1b1eb296a5
叶帆 <yeziahehe@gmail.com>
2020-03-08 23:32:43 +0800

    [026]

---
+++ LeetCode.swift/Solution/026_RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.swift
@@ -0,0 +1,65 @@
+//
+//  RemoveDuplicatesFromSortedArray.swift
+//  LeetCode.swift
+//
+//  Created by 叶帆 on 2020/3/8.
+//  Copyright © 2020 Suzhou Coryphaei Information&Technology Co., Ltd. All rights reserved.
+//
+
+/**
+ Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+
+ Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+ Example 1:
+
+ Given nums = [1,1,2],
+
+ Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+
+ It doesn't matter what you leave beyond the returned length.
+ Example 2:
+
+ Given nums = [0,0,1,1,1,2,2,3,3,4],
+
+ Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+
+ It doesn't matter what values are set beyond the returned length.
+ =====
+ 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+
+ 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+
+  
+
+ 示例 1:
+
+ 给定数组 nums = [1,1,2],
+
+ 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+
+ 你不需要考虑数组中超出新长度后面的元素。
+ 示例 2:
+
+ 给定 nums = [0,0,1,1,1,2,2,3,3,4],
+
+ 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+
+ 你不需要考虑数组中超出新长度后面的元素。
+
+ */
+
+import Foundation
+
+class Solution {
+    func removeDuplicates(_ nums: inout [Int]) -> Int {
+        var ans = 0
+        for i in 0..<nums.count {
+            if nums[i] != nums[ans] {
+                ans += 1
+                nums[ans] = nums[i]
+            }
+        }
+        return min(ans+1, nums.count)
+    }
+}
