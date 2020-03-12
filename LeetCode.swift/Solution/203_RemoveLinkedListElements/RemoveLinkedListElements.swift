0404874d1ad7f59ec93ed6d1936c2a7627df82c5
叶帆 <yeziahehe@gmail.com>
2020-03-12 14:52:59 +0800

    [203]

---
+++ LeetCode.swift/Solution/203_RemoveLinkedListElements/RemoveLinkedListElements.swift
@@ -0,0 +1,50 @@
+//
+//  RemoveLinkedListElements.swift
+//  LeetCode.swift
+//
+//  Created by 叶帆 on 2020/3/12.
+//  Copyright © 2020 Suzhou Coryphaei Information&Technology Co., Ltd. All rights reserved.
+//
+
+/**
+ Remove all elements from a linked list of integers that have value val.
+
+ Example:
+
+ Input:  1->2->6->3->4->5->6, val = 6
+ Output: 1->2->3->4->5
+ =====
+ 删除链表中等于给定值 val 的所有节点。
+
+ 示例:
+
+ 输入: 1->2->6->3->4->5->6, val = 6
+ 输出: 1->2->3->4->5
+ */
+
+import Foundation
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+class Solution {
+    func removeElements(_ head: ListNode?, _ val: Int) -> ListNode? {
+        let dummy = ListNode(-1)
+        dummy.next = head
+        var node: ListNode? = dummy
+        while (node != nil) {
+            if node?.next?.val == val {
+                node?.next = node?.next?.next
+            } else {
+                node = node?.next
+            }
+        }
+        return dummy.next
+    }
+}
