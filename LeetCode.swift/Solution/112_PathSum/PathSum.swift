0c4df9129f8b04efc0b249e0c686c220e79fa0b4
叶帆 <yeziahehe@gmail.com>
2020-04-12 22:53:30 +0800

    [112]

---
+++ LeetCode.swift/Solution/112_PathSum/PathSum.swift
@@ -0,0 +1,69 @@
+//
+//  PathSum.swift
+//  LeetCode.swift
+//
+//  Created by 叶帆 on 2020/4/12.
+//  Copyright © 2020 Suzhou Coryphaei Information&Technology Co., Ltd. All rights reserved.
+//
+
+/**
+ Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+ Note: A leaf is a node with no children.
+
+ Example:
+
+ Given the below binary tree and sum = 22,
+
+       5
+      / \
+     4   8
+    /   / \
+   11  13  4
+  /  \      \
+ 7    2      1
+ return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+ =====
+ 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+
+ 说明: 叶子节点是指没有子节点的节点。
+
+ 示例:
+ 给定如下二叉树，以及目标和 sum = 22，
+
+               5
+              / \
+             4   8
+            /   / \
+           11  13  4
+          /  \      \
+         7    2      1
+ 返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+
+ */
+
+import Foundation
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
+
+class Solution {
+    func hasPathSum(_ root: TreeNode?, _ sum: Int) -> Bool {
+        if root == nil {
+            return false
+        }
+        
+        if root?.left == nil && root?.right == nil {
+            return (sum - root!.val) == 0
+        }
+        return hasPathSum(root?.left, sum - root!.val) || hasPathSum(root?.right, sum - root!.val)
+    }
+}
