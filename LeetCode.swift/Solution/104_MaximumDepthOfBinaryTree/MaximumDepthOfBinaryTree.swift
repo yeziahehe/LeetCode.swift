463f373603f5c3dcf9c9029c8326ce004dea0124
叶帆 <yeziahehe@gmail.com>
2020-04-12 21:24:37 +0800

    [104]

---
+++ LeetCode.swift/Solution/104_MaximumDepthOfBinaryTree/MaximumDepthOfBinaryTree.swift
@@ -0,0 +1,67 @@
+//
+//  MaximumDepthOfBinaryTree.swift
+//  LeetCode.swift
+//
+//  Created by 叶帆 on 2020/4/12.
+//  Copyright © 2020 Suzhou Coryphaei Information&Technology Co., Ltd. All rights reserved.
+//
+
+/**
+ Given a binary tree, find its maximum depth.
+
+ The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+ Note: A leaf is a node with no children.
+
+ Example:
+
+ Given binary tree [3,9,20,null,null,15,7],
+
+     3
+    / \
+   9  20
+     /  \
+    15   7
+ return its depth = 3.
+ =====
+ 给定一个二叉树，找出其最大深度。
+
+ 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+ 说明: 叶子节点是指没有子节点的节点。
+
+ 示例：
+ 给定二叉树 [3,9,20,null,null,15,7]，
+
+     3
+    / \
+   9  20
+     /  \
+    15   7
+ 返回它的最大深度 3 。
+
+ */
+
+import Foundation
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+}
+
+class Solution {
+    func maxDepth(_ root: TreeNode?) -> Int {
+        if root == nil {
+            return 0
+        }
+        let left = maxDepth(root!.left)
+        let right = maxDepth(root!.right)
+        return max(left, right) + 1
+    }
+}
