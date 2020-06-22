57c512057efb92ba37bee9fb691eb20968ff6ff4
叶帆 <yeziahehe@gmail.com>
2020-06-22 15:47:51 +0800

    feat: 🎸 70 Climbing Stairs

---
+++ LeetCode.swift/Solution/070_ClimbingStairs/ClimbingStairs.swift
@@ -0,0 +1,71 @@
+//
+//  ClimbingStairs.swift
+//  LeetCode.swift
+//
+//  Created by 叶帆 on 2020/6/22.
+//  Copyright © 2020 Suzhou Coryphaei Information&Technology Co., Ltd. All rights reserved.
+//
+
+/**
+ You are climbing a stair case. It takes n steps to reach to the top.
+
+ Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ Note: Given n will be a positive integer.
+
+ Example 1:
+
+ Input: 2
+ Output: 2
+ Explanation: There are two ways to climb to the top.
+ 1. 1 step + 1 step
+ 2. 2 steps
+ Example 2:
+
+ Input: 3
+ Output: 3
+ Explanation: There are three ways to climb to the top.
+ 1. 1 step + 1 step + 1 step
+ 2. 1 step + 2 steps
+ 3. 2 steps + 1 step
+
+ ======
+ 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+
+ 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+
+ 注意：给定 n 是一个正整数。
+
+ 示例 1：
+
+ 输入： 2
+ 输出： 2
+ 解释： 有两种方法可以爬到楼顶。
+ 1.  1 阶 + 1 阶
+ 2.  2 阶
+ 示例 2：
+
+ 输入： 3
+ 输出： 3
+ 解释： 有三种方法可以爬到楼顶。
+ 1.  1 阶 + 1 阶 + 1 阶
+ 2.  1 阶 + 2 阶
+ 3.  2 阶 + 1 阶
+
+ */
+import Foundation
+
+class Solution {
+    func climbStairs(_ n: Int) -> Int {
+        if n < 3 {
+            return n
+        }
+        var dp: [Int] = Array(repeating: 0, count: n + 1)
+        dp[1] = 1
+        dp[2] = 2
+        for i in 3...n {
+            dp[i] = dp[i-1] + dp[i-2]
+        }
+        return dp[n]
+    }
+}
