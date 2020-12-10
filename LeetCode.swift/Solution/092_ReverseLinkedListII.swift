8835db6227cf4c9e8e20f4bc6060b5b6eac211da
叶帆 <yeziahehe@gmail.com>
2020-12-10 11:13:28 +0800

    docs: ✏️ 92. 反转链表 II 添加注释

--- LeetCode.swift/Solution/092_ReverseLinkedListII.swift
+++ LeetCode.swift/Solution/092_ReverseLinkedListII.swift
@@ -24,23 +24,31 @@
 import Foundation
 
 class Solution {
-    var successor: ListNode?
+    var successor: ListNode? // 后驱节点
 
+    // 反转以 head 为起点的 n 个节点，返回新的头结点
     func reverseN(_ head: ListNode?, _ n: Int) -> ListNode? {
+        // base case
         if n == 1 {
+            // 记录第 n + 1 个节点
             successor = head?.next
             return head
         }
+        // 以 head.next 为起点，需要反转前 n - 1 个节点
         let last = reverseN(head?.next, n - 1)
         head?.next?.next = head
+        // 让反转之后的 head 节点和后面的节点连起来
         head?.next = successor
         return last
     }
 
     func reverseBetween(_ head: ListNode?, _ m: Int, _ n: Int) -> ListNode? {
+        // base case
         if m == 1 {
+            // 相当于反转前 n 个元素
             return reverseN(head, n)
         }
+        // 前进到反转的起点触发 base case
         head?.next = reverseBetween(head?.next, m - 1, n - 1)
         return head
     }
