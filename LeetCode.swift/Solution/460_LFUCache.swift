74f3090ff62280837af02a1060d45ca6824a21b7
叶帆 <yeziahehe@gmail.com>
2020-12-16 17:12:03 +0800

    feat: 🎸 875. 爱吃香蕉的珂珂

---
+++ LeetCode.swift/Solution/460_LFUCache.swift
@@ -0,0 +1,9 @@
+//
+//  460_LFUCache.swift
+//  LeetCode.swift
+//
+//  Created by 叶帆 on 2020/12/16.
+//  Copyright © 2020 Suzhou Coryphaei Information&Technology Co., Ltd. All rights reserved.
+//
+
+import Foundation
---
+++ LeetCode.swift/Solution/875_KokoEatingBananas.swift
@@ -0,0 +1,73 @@
+//
+//  875_KokoEatingBananas.swift
+//  LeetCode.swift
+//
+//  Created by 叶帆 on 2020/12/16.
+//  Copyright © 2020 Suzhou Coryphaei Information&Technology Co., Ltd. All rights reserved.
+//
+
+/**
+ 珂珂喜欢吃香蕉。这里有 N 堆香蕉，第 i 堆中有 piles[i] 根香蕉。警卫已经离开了，将在 H 小时后回来。
+
+ 珂珂可以决定她吃香蕉的速度 K （单位：根/小时）。每个小时，她将会选择一堆香蕉，从中吃掉 K 根。如果这堆香蕉少于 K 根，她将吃掉这堆的所有香蕉，然后这一小时内不会再吃更多的香蕉。
+
+ 珂珂喜欢慢慢吃，但仍然想在警卫回来前吃掉所有的香蕉。
+
+ 返回她可以在 H 小时内吃掉所有香蕉的最小速度 K（K 为整数）。
+
+  
+
+ 示例 1：
+
+ 输入: piles = [3,6,7,11], H = 8
+ 输出: 4
+ 示例 2：
+
+ 输入: piles = [30,11,23,4,20], H = 5
+ 输出: 30
+ 示例 3：
+
+ 输入: piles = [30,11,23,4,20], H = 6
+ 输出: 23
+  
+
+ 提示：
+
+ 1 <= piles.length <= 10^4
+ piles.length <= H <= 10^9
+ 1 <= piles[i] <= 10^9
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/koko-eating-bananas
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+import Foundation
+
+class Solution {
+    func minEatingSpeed(_ piles: [Int], _ H: Int) -> Int {
+        var maxVal = 0
+        for pile in piles {
+            maxVal = max(maxVal, pile)
+        }
+        var left = 1
+        var right = maxVal + 1
+        while left < right {
+            let mid = left + (right - left) / 2
+            if canFinish(piles, H, mid) {
+                right = mid
+            } else {
+                left = mid + 1
+            }
+        }
+        return left
+    }
+
+    // 时间复杂度 O(N)
+    func canFinish(_ piles: [Int], _ H: Int, _ speed: Int) -> Bool {
+        var time = 0
+        for pile in piles {
+            time += (pile / speed) + ((pile % speed) > 0 ? 1 : 0)
+        }
+        return time <= H
+    }
+}
